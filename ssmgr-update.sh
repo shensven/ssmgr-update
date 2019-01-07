32ccebd4e8d18695f00639234de3d1da27d35191
shensven <shensven2016@gmail.com>
2019-01-07 23:34:44 +0800

    Update ssmgr-update.sh

--- ssmgr-update.sh
+++ ssmgr-update.sh
@@ -4,10 +4,19 @@ update_ssmgr(){
 	npm i -g shadowsocks-manager --unsafe-perm
 }
 
+start_ssmgr(){
+	pm2 start all
+}
+
+stop_ssmgr(){
+	pm2 stop all
+}
+
 restart_ssmgr(){
 	pm2 restart all
 }
 
+stop_ssmgr
 update_ssmgr
 restart_ssmgr
 
